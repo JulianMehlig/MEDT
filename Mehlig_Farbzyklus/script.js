4abbe186f1f931e5534050ae51b56debedaaf455
Julian Mehlig <32087968+JulianMehlig@users.noreply.github.com>
2017-12-06 08:05:50 +0100

    Farbzyklus

--- Mehlig_Farbzyklus/script.js
+++ Mehlig_Farbzyklus/script.js
@@ -1,6 +1,25 @@
-function display()
-{
-    var col=document.getElementById("demo");
-    col.style.color="#FF0000";
-}
+var box = document.getElementsByClassName("box");
 
+var counter = 1;
+
+
+
+setInterval(function ()
+{
+    changeBG(counter);
+    counter += 1;
+    if(counter>3)
+    {
+        counter = 1;
+    }},1000);
+
+
+function changeBG(counter)
+{
+    var color = new Array();
+    color[1] = 'white';
+    color[2] = 'blue';
+    color[3] = 'green';
+
+    box[0].style.backgroundColor = color[counter];
+}
